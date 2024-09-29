df9620271fdbccb8f9280252c5b0d9e6fc8baff1
Artur Finger <fingerartur@gmail.com>
2024-09-30 01:44:43 +0300

    Fix baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,17 +7,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Exper',
-  tagline: 'Experience, expertise and know how',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/build/',
+  tagline: 'Experience, expertise and knowhow',
+  url: 'https://fingerartur.github.io/',
+  baseUrl: '/exper/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: '', // Usually your GitHub org/user name.
+  projectName: 'exper', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
